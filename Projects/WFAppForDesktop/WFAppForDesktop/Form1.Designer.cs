d0cf6db394fccf08171bb09c665294a29fe77d90
Paul Malyarevich <paul.lightit@gmail.com>
2017-05-22 08:09:13 +0300

    init

---
+++ Projects/WFAppForDesktop/WFAppForDesktop/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WFAppForDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(320, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите параметры поиска";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(371, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(323, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Old";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(323, 56);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(43, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "City";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(371, 52);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Найдено:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 421);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WebSites/CourseworkBD/Scripts/script.js
@@ -0,0 +1,188 @@
+
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+jQuery(function() {
+    jQuery(window).bind("load resize", function() {
+        topOffset = 50;
+        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            jQuery('div.navbar-collapse').addClass('collapse')
+            topOffset = 100; // 2-row-menu
+        } else {
+            jQuery('div.navbar-collapse').removeClass('collapse')
+        }
+
+        height = (this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            jQuery("#page-wrapper").css("min-height", (height) + "px");
+        }
+    })
+});
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+var customFunc = function customFunction() {
+    switchVs();
+    jQuery(document).on('click','#profileToggle', function(){
+        jQuery(this).find('.btn').toggleClass('active');
+
+        if (jQuery(this).find('.btn-primary').size()>0) {
+            switchVs("parentInfo", "personalInfo");
+            jQuery(this).find('.btn').toggleClass('btn-primary');
+        }
+        jQuery(this).find('.btn').toggleClass('btn-default');
+    });
+}
+
+function ready() {
+    
+    customFunc();
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
+jQuery("#side-menu>li>a").click(function(evt) {
+    jQuery("#side-menu>li>a").removeClass("active");
+    jQuery(this).addClass("active");
+    console.log(this.hash);
+});
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+chw = function chooseHeightWeight() {
+    jQuery('#span_height').click(function() {
+        jQuery('#span_height').hide();
+        jQuery('#height_input').show();
+        sethw();
+    });
+    jQuery('#span_weight').click(function() {
+        jQuery('#span_weight').hide();
+        jQuery('#weight_input').show();
+        sethw();
+    });
+}
+
+sethw = function setHeightWeight() {
+    jQuery(document).ready(function() {
+        jQuery('#height_input').keydown(function(e) {
+            if (e.keyCode === 13) {
+                jQuery('#height_input').hide();
+                jQuery('#span_height').show();
+            }
+        });
+    });
+
+    jQuery(document).ready(function() {
+        jQuery('#weight_input').keydown(function(e) {
+            if (e.keyCode === 13) {
+                jQuery('#weight_input').hide();
+                jQuery('#span_weight').show();
+            }
+        });
+    });
+}
+
+sdii = function setDataInInputs() {
+    jQuery('#table_med tr').click(function() {
+        jQuery('#cancel_vac').show();
+        jQuery(this).find('td').each(function() {
+            var value = jQuery(this).html();
+            var type = jQuery(this).attr('type');
+
+            jQuery('.inputs #' + type).val(value);
+            jQuery("#select_type_vaccination :contains(" + value + ")").prop("selected", true);
+            jQuery("#select_reason_vaccination :contains(" + value + ")").prop("selected", true);
+        });
+    });
+}
+
+gd = function getDataFromImputs() {
+    jQuery("#submit_vaccination_info").click(function() {
+        var type = jQuery("#select_type_vaccination :selected").val(); //val
+        var reason = jQuery("#select_reason_vaccination :selected").text();
+        var date = jQuery("#date_vac").val();
+        var term = jQuery("#term_vac").val();
+        jQuery('#cancel_vac').hide();
+        clear();
+        return false;
+    });
+    jQuery("#cancel_vac").click(function() {
+        jQuery('#cancel_vac').hide();
+        clear();
+    });
+}
+
+clear = function clearVacInputs() {
+    jQuery("#select_type_vaccination :first").prop("selected", true);
+    jQuery("#select_reason_vaccination :first").prop("selected", true);
+    jQuery("#date_vac").val('');
+    jQuery("#term_vac").val('');
+}
+
+
+// -----------------------------------Lesha---------------------------------------
+
+ switchVs=function switchVisible(firstBlock, secondBlock) {
+     dcRdy();
+    if (document.getElementById(firstBlock)) {
+        if (document.getElementById(firstBlock).style.display == 'none') {
+            document.getElementById(firstBlock).style.display = 'block';
+            document.getElementById(secondBlock).style.display = 'none';
+        }
+        else {
+            document.getElementById(firstBlock).style.display = 'none';
+            document.getElementById(secondBlock).style.display = 'block';
+        }
+    }
+}
+
+dcRdy = function dcReady() {
+    pageSize = 1;
+    pagesCount = jQuery(".content").length;
+    var currentPage = 1;
+
+    /////////// PREPARE NAV ///////////////
+    var nav = '';
+    var totalPages = Math.ceil(pagesCount / pageSize);
+    for (var s=0; s<totalPages; s++){
+        nav += '<li class="numeros"><a href="#">'+(s+1)+'</a></li>';
+    }
+    jQuery(".pag_prev").after(nav);
+    jQuery(".numeros").first().addClass("active");
+    //////////////////////////////////////
+
+    showPage = function() {
+        jQuery(".content").hide().each(function(n) {
+            if (n >= pageSize * (currentPage - 1) && n < pageSize * currentPage)
+                jQuery(this).show();
+        });
+    }
+    showPage();
+
+    jQuery(".pagination li.numeros").click(function() {
+        jQuery(".pagination li").removeClass("active");
+        jQuery(this).addClass("active");
+        currentPage = parseInt($(this).text());
+        showPage();
+    });
+
+    jQuery(".pagination li.pag_prev").click(function() {
+        if(jQuery(this).next().is('.active')) return;
+        jQuery('.numeros.active').removeClass('active').prev().addClass('active');
+        currentPage = currentPage > 1 ? (currentPage-1) : 1;
+        showPage();
+    });
+
+    jQuery(".pagination li.pag_next").click(function() {
+        if(jQuery(this).prev().is('.active')) return;
+        jQuery('.numeros.active').removeClass('active').next().addClass('active');
+        currentPage = currentPage < totalPages ? (currentPage+1) : totalPages;
+        showPage();
+    });
+}
